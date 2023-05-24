49d2a6b2f27ab3b23688c2845caf2246bef8761e
peiqiao623 <83063137+peiqiao623@users.noreply.github.com>
2023-05-24 18:27:28 +1000

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
+def on_received_number(receivedNumber):
+    global hp
+    hp += 0 - receivedNumber
+radio.on_received_number(on_received_number)
+
 def on_button_pressed_a():
     global hunger
     if attack_mode == 1:
         radio.send_number(1)
         hunger += -3
     else:
-        basic.show_string("Hunger", 150)
-        basic.show_string("" + str(hunger))
+        basic.show_string("Hunger:" + str(hunger))
+        basic.show_string("HP:" + str(hp))
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_gesture_free_fall():
-    global fun, hunger
-    fun += 1
-    hunger += -2
-input.on_gesture(Gesture.FREE_FALL, on_gesture_free_fall)
+def on_gesture_logo_up():
+    global hunger, fun
+    hunger += 1
+    fun += -2
+input.on_gesture(Gesture.LOGO_UP, on_gesture_logo_up)
 
 def on_button_pressed_ab():
     global attack_mode
@@ -28,21 +33,40 @@ def on_button_pressed_b():
         radio.send_number(4)
         hunger += -10
     else:
-        pass
+        basic.show_string("Fun:" + str(fun))
+        basic.show_string("Money:" + str(money))
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_gesture_shake():
-    global hunger, fun
-    hunger += 1
-    fun += -2
+    global fun, hunger
+    fun += 1
+    hunger += -2
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
 attack_mode = 0
+money = 0
+fun = 0
 hunger = 0
+hp = 0
 radio.set_group(1)
+hp = 10
 hunger = 10
 fun = 10
 money = 10
 fun = 0
 attack_mode = 0
 images.icon_image(IconNames.RABBIT).show_image(0)
+
+def on_forever():
+    global money, hp
+    if 0 > hp:
+        money += -10
+    elif 20 < hunger:
+        money += 4
+    elif 0 > hunger:
+        hp += -1
+    else:
+        pass
+    if 0 > money:
+        basic.show_string("Game Over")
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -6,13 +6,13 @@ input.onButtonPressed(Button.A, function () {
         radio.sendNumber(1)
         hunger += -3
     } else {
-        basic.showString("Hunger", 150)
-basic.showString("" + hunger)
+        basic.showString("Hunger:" + hunger)
+        basic.showString("HP:" + hp)
     }
 })
-input.onGesture(Gesture.FreeFall, function () {
-    fun += 1
-    hunger += -2
+input.onGesture(Gesture.LogoUp, function () {
+    hunger += 1
+    fun += -2
 })
 input.onButtonPressed(Button.AB, function () {
     if (attack_mode == 0) {
@@ -26,20 +26,38 @@ input.onButtonPressed(Button.B, function () {
         radio.sendNumber(4)
         hunger += -10
     } else {
-    	
+        basic.showString("Fun:" + fun)
+        basic.showString("Money:" + money)
     }
 })
 input.onGesture(Gesture.Shake, function () {
-    hunger += 1
-    fun += -2
+    fun += 1
+    hunger += -2
 })
-let hp = 0
 let attack_mode = 0
+let money = 0
+let fun = 0
 let hunger = 0
+let hp = 0
 radio.setGroup(1)
+hp = 10
 hunger = 10
-let fun = 10
-let money = 10
+fun = 10
+money = 10
 fun = 0
 attack_mode = 0
 images.iconImage(IconNames.Rabbit).showImage(0)
+basic.forever(function () {
+    if (0 > hp) {
+        money += -10
+    } else if (20 < hunger) {
+        money += 4
+    } else if (0 > hunger) {
+        hp += -1
+    } else {
+    	
+    }
+    if (0 > money) {
+        basic.showString("Game Over")
+    }
+})
